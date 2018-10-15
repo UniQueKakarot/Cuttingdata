78b689984e72356373f913fcd8a96c12ca891fcb
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2018-10-15 20:09:03 +0200

    I dont like this

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ class Cuttingdata(tk.Frame):
         self.master = master
         self.font_size = ("Consolas", 23)
         self.font_size1 = ("San Francisco", 25)
-        master_frame = tk.Frame(self.master, bg='Grey', width='800', height='600').grid(row=0, column=0, rowspan=10, columnspan=10)
+
+        tab_controll = ttk.Notebook(self.master)
+        tab1 = ttk.Frame(tab_controll)
+        tab_controll.add(tab1, text='Tab 1')
+        tab_controll.grid(row=0, column=0, rowspan=10, columnspan=2)
+
+        master_frame = tk.Frame(tab1, bg='white', width='900', height='700').grid(row=0, column=0)
         #master_frame.grid(row=0, column=0)
 
         #self.preview_body(master_frame)
         self.asset_preview_body(master_frame)
+        
 
     def preview_body(self, master):
 
@@ -50,7 +57,7 @@ class Cuttingdata(tk.Frame):
 
         label1 = tk.Label(master, bg='Grey', image=header)
         label1.image = header
-        label1.grid(row=0, column=0, columnspan=2, sticky='N', pady=5)
+        label1.grid(row=0, column=0, columnspan=2, sticky='S', pady=5)
 
         frame = Path('./Assets/Labelframe3.png')
         labelframe = tk.PhotoImage(file=frame)
@@ -60,9 +67,11 @@ class Cuttingdata(tk.Frame):
 
             label2 = tk.Label(master, bg='Grey', image=labelframe, text=text, compound='center', font=self.font_size)
             label2.image = labelframe
-            label2.grid(row=row, column=0, sticky='W')
+            label2.grid(row=row, column=0, sticky='W', padx=5, in_=master)
 
-            tk.Entry(master, bg='#737373', font=self.font_size1, relief='flat', justify='center').grid(row=row, column=1, sticky='EW', padx=5)
+            tk.Entry(master, bg='#737373', font=self.font_size1, relief='flat', justify='center').grid(row=row, column=1, sticky='EW', padx=8)
+
+        tk.Button(master, bg='#737373', font=self.font_size1, text='JUST DO IT!').grid(row=6, column=0, columnspan=2, sticky='EW', padx=15)
 
 
 
@@ -70,7 +79,7 @@ class Cuttingdata(tk.Frame):
 
 
 win = tk.Tk()
-win.geometry("800x600")
+win.geometry("900x700")
 #win.resizable(width=False, height=False)
 
 app = Cuttingdata(master=win)
